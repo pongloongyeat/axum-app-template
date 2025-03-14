78bf04adc7155ea168654e5f848b148e409fb350
Pong Loong Yeat <pongloongyeat@gmail.com>
2025-03-14 17:05:55 +0800

    Try dioxus as the frontend

---
+++ frontend/src/main.rs
@@ -0,0 +1,45 @@
+use dioxus::prelude::*;
+
+#[derive(Debug, Clone, Routable, PartialEq)]
+#[rustfmt::skip]
+enum Route {
+    #[layout(Navbar)]
+    #[route("/")]
+    Home {},
+}
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+const TAILWIND_CSS: &'static str = "https://cdn.tailwindcss.com";
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Script { src: TAILWIND_CSS }
+        Router::<Route> {}
+    }
+}
+
+/// Home page
+#[component]
+fn Home() -> Element {
+    rsx! { "Hello world!" }
+}
+
+/// Shared navbar component.
+#[component]
+fn Navbar() -> Element {
+    rsx! {
+        div { id: "navbar",
+            Link { to: Route::Home {}, "Home" }
+        }
+
+        Outlet::<Route> {}
+    }
+}
